30a35541d627580dc0377b7103ecd40d1d667c49
joppevsb <joppevsb@icloud.com>
2025-02-05 23:24:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,30 @@
-function selectOption(option) {
-    if (option === 'yes') {
-        flashRainbowColors(function() {
-            document.getElementById('question').style.display = 'none';
-            document.getElementById('secret-message').classList.remove('hidden');
-            startSlideshow();
-        });
-    } else if (option === 'no') {
-        document.getElementById('no-button').innerText = 'You sure?';
-        var yesButton = document.getElementById('yes-button');
-        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
-        var newSize = parseFloat(currentFontSize) * 2;
-        yesButton.style.fontSize = newSize + 'px';
-    }
+// Array met afbeeldingennamen voor de slideshow
+const images = [
+    "image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", 
+    "image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg",
+    "image9.jpg", "image10.jpg", "image11.jpg"
+];
+
+let currentImageIndex = 0;
+const slide = document.getElementById("slide");
+
+// Laat de volgende afbeelding zien in de slideshow
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    slide.src = images[currentImageIndex];
 }
 
-// Regenboog effect
-function flashRainbowColors(callback) {
-    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
-    var i = 0;
-    var interval = setInterval(function() {
-        document.body.style.backgroundColor = colors[i];
-        i = (i + 1) % colors.length;
-    }, 200);
-    setTimeout(function() {
-        clearInterval(interval);
-        document.body.style.backgroundColor = '';
-        if (callback) {
-            callback();
-        }
-    }, 2000);
-}
+// Start slideshow met interval
+let interval;
 
-// Draaiende kat links
-function displayLeftCat() {
-    var leftCatContainer = document.getElementById('left-cat-container');
-    var leftCatImage = new Image();
-    leftCatImage.src = 'cat.gif';
-    leftCatImage.alt = 'Cat';
-    leftCatImage.style.animation = "spin 4s linear infinite"; 
-    leftCatImage.onload = function() {
-        leftCatContainer.appendChild(leftCatImage);
-    };
-}
+// Knop "Yes" - laat slideshow zien en start animatie
+document.getElementById("yesButton").addEventListener("click", function() {
+    document.getElementById("question-container").classList.add("hidden"); // Verberg de vraag
+    document.querySelector(".slideshow").style.display = "block"; // Toon de slideshow
+    interval = setInterval(nextImage, 2000); // Verander elke 2 seconden
+});
 
-// Schattige kat rechts
-function displayRightCat() {
-    var rightCatContainer = document.getElementById('right-cat-container');
-    var rightCatImage = new Image();
-    rightCatImage.src = 'cutecat.gif';
-    rightCatImage.alt = 'Cute Cat';
-    rightCatImage.onload = function() {
-        rightCatContainer.appendChild(rightCatImage);
-    };
-}
-
-// Start slideshow
-function startSlideshow() {
-    var slideshow = document.getElementById('slideshow');
-    slideshow.style.display = 'block';
-
-    var images = [];
-    for (var i = 1; i <= 11; i++) {
-        images.push('image' + i + '.jpg');
-    }
-
-    var index = 0;
-    setInterval(function() {
-        index = (index + 1) % images.length;
-        slideshow.src = images[index];
-    }, 2000);
-}
-
-// Start de katten bij het laden van de pagina
-displayLeftCat();
-displayRightCat();
+// Knop "No" - laat een bericht zien
+document.getElementById("noButton").addEventListener("click", function() {
+    alert("Aww, maybe next time! 💔");
+});
